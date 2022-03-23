85e98b25c7f86263316564b53c45e2f869ea64eb
masudx08 <promasud225@gmail.com>
2022-03-23 20:53:48 +0600

    done

--- src/components/component/components/Header/Header.js
+++ src/components/component/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Coin1 from "../../assets/img/coin/1.png"
 import Coin2 from "../../assets/img/coin/2.png"
 import Coin3 from "../../assets/img/coin/3.png"
@@ -12,6 +12,7 @@ import { Button, Modal } from 'react-bootstrap'
 
 
 function MyVerticallyCenteredModal(props) {
+    
     return (
         <Modal
             {...props}
@@ -28,6 +29,7 @@ function MyVerticallyCenteredModal(props) {
 
 export default function Header() {
     const [modalShow, setModalShow] = React.useState(false);
+    const [bnb, setBnb] = useState(1)
     return (
         <div>
             <MyVerticallyCenteredModal
@@ -37,7 +39,7 @@ export default function Header() {
 
             <div className="banner headerBanner" id="transfer">
 
-                <div className="man">
+                {/* <div className="man">
                     <img src={Coin1} alt="" className="coin-1" />
                     <img src={Coin2} alt="" className="coin-2" />
                     <img src={Coin3} alt="" className="coin-3" />
@@ -47,7 +49,7 @@ export default function Header() {
                     <img src={Coin7} alt="" className="coin-7" />
                     <img src={Coin8} alt="" className="coin-8" />
                     <img className="bg-sape-1" src={BannerBg2} alt="" />
-                </div>
+                </div> */}
                 <div className="hero-area">
                     <div className="container">
                         <div className="row justify-content-between">
@@ -80,157 +82,15 @@ export default function Header() {
                                 </div>
                             </div>
                             <div className="col-lg-5 col-xl-cus wow fadeInUp" data-wow-duration="0.3s" data-wow-delay="0.3s">
-                                {/* <div className="right-box">
-                        
-                            <div className="tab-content" id="pills-tabContent">
-                            
-                                <div className="tab-pane fade show active" id="pills-home" role="tabpanel"
-                                    aria-labelledby="pills-home-tab">
-                                    <div className="exchange">
-                                    <div className="exchange-box">
-                                            <form action="#">
-                                                <div className="row">
-                                                    <div className="col-md-12 gx-2">
-                                                        <div className="form-group">
-                                                            <input type="number" placeholder=" Total BNB available to invest"
-                                                                className="form-control" />
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-                                                <div className="row">
-                                                    <div className="col-md-6 gx-2">
-                                                        <div className="form-group">
-                                                            <input type="number" placeholder="Enter BNB Amount"
-                                                                className="form-control" />
-                                                        </div>
-                                                    </div>
-                                                    <div className="col-md-6 gx-2">
-                                                        <div className="form-group">
-                                                            <div className="selector">
-                                                                <div className="coin">
-                                                                    <img src="./assets/img/btc.png" alt="" />
-                                                                    <div className="language-select">
-                                                                        <select className="select-bar">
-                                                                            <option value="">BTC</option>
-                                                                            <option value="">USD</option>
-                                                                            <option value="">BTC</option>
-                                                                        </select>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </form>
-                                            <div className="button-box">
-                                            <a href="#" className="button button-1">Invest Now !</a>
-                                        </div>
-                                        </div>
-                                        <div className="exchange-box">
-                                            <form action="#">
-                                                <div className="row">
-                                                    <div className="col-md-12 gx-2">
-                                                        <div className="form-group">
-                                                            <input type="text" placeholder="Profit ($)"
-                                                                className="form-control" />
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                               
-                                                
-                                            </form>
-                                        </div>
-
-                                        
-                                       
-                                    </div>
-                                </div>
-                                <div className="tab-pane fade" id="pills-profile" role="tabpanel"
-                                    aria-labelledby="pills-profile-tab">
-                                    <div className="exchange">
-                                    <div className="exchange-box">
-                                            <form action="#">
-                                                <div className="row">
-                                                    <div className="col-md-12 gx-2">
-                                                        <div className="form-group">
-                                                            <input type="number" placeholder=" Total BNB available to invest"
-                                                                className="form-control" />
-                                                        </div>
-                                                    </div>
-                                                </div>
-
-                                                <div className="row">
-                                                    <div className="col-md-6 gx-2">
-                                                        <div className="form-group">
-                                                            <input type="number" placeholder="Enter BNB Amount"
-                                                                className="form-control" />
-                                                        </div>
-                                                    </div>
-                                                    <div className="col-md-6 gx-2">
-                                                        <div className="form-group">
-                                                            <div className="selector">
-                                                                <div className="coin">
-                                                                    <img src="./assets/img/btc.png" alt="" />
-                                                                    <div className="language-select">
-                                                                        <select className="select-bar">
-                                                                            <option value="">BTC</option>
-                                                                            <option value="">USD</option>
-                                                                            <option value="">BTC</option>
-                                                                        </select>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </form>
-                                            <div className="button-box">
-                                            <a href="#" className="button button-1">Invest Now !</a>
-                                        </div>
-                                        </div>
-                                        <div className="exchange-box">
-                                            <form action="#">
-                                                <div className="row">
-                                                    <div className="col-md-12 gx-2">
-                                                        <div className="form-group">
-                                                            <input type="text" placeholder="User Profit ($)"
-                                                                className="form-control" />
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                               
-                                                
-                                            </form>
-                                        </div>
-
-                                        
-                                       
-                                    </div>
-                                </div>
-                            </div>
-                            <ul className="nav nav-pills pt-3" id="pills-tab" role="tablist" style={{width:'80%', margin:'auto'}}>
-                                <li className="nav-item" role="presentation">
-                                    <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill"
-                                        data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home"
-                                        aria-selected="true">Reinvest</button>
-                                </li>
-                                <li className="nav-item" role="presentation">
-                                    <button className="nav-link" id="pills-profile-tab" data-bs-toggle="pill"
-                                        data-bs-target="#pills-profile" type="button" role="tab"
-                                        aria-controls="pills-profile" aria-selected="false">Claim</button>
-                                </li>
-                            </ul>
-                            
-                        </div> */}
+                                
                                 <div className="right-box">
                                     <ul className="nav nav-pills" id="pills-tab" role="tablist">
                                         <li className="nav-item" role="presentation">
-                                            <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Earn Interest</button>
+                                            <button className="nav-link active" id="pills-home-tab" data-bs-toggle="pill" data-bs-target="#pills-home" type="button" role="tab" aria-controls="pills-home" aria-selected="true">Connect Wallet</button>
                                         </li>
-                                        <li className="nav-item" role="presentation">
+                                        {/* <li className="nav-item" role="presentation">
                                             <button className="nav-link" id="pills-profile-tab" data-bs-toggle="pill" data-bs-target="#pills-profile" type="button" role="tab" aria-controls="pills-profile" aria-selected="false">Get Loan</button>
-                                        </li>
+                                        </li> */}
                                     </ul>
                                     <div className="tab-content" id="pills-tabContent">
                                         <div className="tab-pane fade show active" id="pills-home" role="tabpanel" aria-labelledby="pills-home-tab">
@@ -242,19 +102,19 @@ export default function Header() {
                                                             <img src="./assets/img/btc.png" alt="" />
                                                             <div className="language-select">
                                                                 <select className="select-bar" style={{display:'none'}}>
-                                                                    <option value="btc">BTC</option>
+                                                                    <option value="btc">BNB</option>
                                                                     <option value="usd">USD</option>
                                                                     <option value="btc">BTC</option>
-                                                                </select><div className="nice-select select-bar" tabIndex="0"><span className="current">BTC</span><ul className="list"><li data-value="" className="option selected focus">BTC</li><li data-value="" className="option">USD</li><li data-value="" className="option">BTC</li></ul></div>
+                                                                </select><div className="nice-select select-bar" tabIndex="0"><span className="current">BNB</span><ul className="list"><li data-value="" className="option selected focus">BNB</li><li data-value="" className="option">USD</li><li data-value="" className="option">BTC</li></ul></div>
                                                             </div>
                                                         </div>
                                                     </div>
                                                     <form action="#">
                                                         <div className="form-group">
-                                                            <input type="text" name="number" placeholder="100" className="form-control" />
+                                                            <input type="text" name="number" placeholder="1 BNB" className="form-control" onChange={(e)=>setBnb(e.target.value)} />
                                                         </div>
                                                     </form>
-                                                    <span className="rate">1 BTC = 11724.597423 USD</span>
+                                                    <span className="rate">Minimum 0.05 BNB</span>
                                                 </div>
                                                 <div className="exchange-box">
                                                     <div className="selector">
@@ -273,19 +133,17 @@ export default function Header() {
 
                                                 <div className="content">
                                                     <div className="text-box">
-                                                        <p className="text"> <span className="one">Deposit Amount</span> <span className="two">100
-                                                            BTC</span></p>
-                                                        <p className="text"> <span className="one">Annual Interest</span> <span className="two">9.4%
-                                                            BTC</span></p>
+                                                        <p className="text"> <span className="one">Daily ROI</span> <span className="two">10% BNB</span></p>
+                                                        <p className="text"> <span className="one">Total Profit</span> <span className="two">300% BNB</span></p>
                                                     </div>
                                                     <div className="earn">
-                                                        <p className="text">You’ll Earn</p>
-                                                        <p className="text two">9.4 BTC</p>
+                                                        <p className="text">You’ll Earn in 30 days</p>
+                                                        <p className="text two">{bnb*3} BNB</p>
                                                     </div>
                                                 </div>
 
                                                 <div className="button-box">
-                                                    <a href="#" className="button button-1">Start Earning Now</a>
+                                                    <a href="#" className="button button-1">Stake Now</a>
                                                 </div>
                                             </div>
                                         </div>
